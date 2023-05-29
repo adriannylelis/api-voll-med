e0c572027f526bbd215d2bb0aa2f7b0adc545119
Adrianny Lelis <adriannylelis@MacBook-de-Adrianny.local>
2023-05-29 16:05:30 -0300

    :recycle: refactor: Status codes e responses

---
+++ src/main/java/med/vol/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,20 @@
+package med.vol.api.paciente;
+
+import med.vol.api.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        Endereco endereco) {
+        public DadosDetalhamentoPaciente(Paciente paciente) {
+            this(
+                    paciente.getNome(),
+                    paciente.getEmail(),
+                    paciente.getTelefone(),
+                    paciente.getCpf(),
+                    paciente.getEndereco()
+            );
+        }
+}
